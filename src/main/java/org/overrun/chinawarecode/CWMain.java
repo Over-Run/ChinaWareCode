82fb880a7ac0d4a93e5169ec692c185cdc2c2cab
baka4n <qq474899581@gmail.com>
2023-03-12 16:09:07 +0800

    初步尝试些lexer

--- src/main/java/org/overrun/chinawarecode/CWMain.java
+++ src/main/java/org/overrun/chinawarecode/CWMain.java
@@ -1,8 +1,11 @@
 package org.overrun.chinawarecode;
 
 
+import org.overrun.chinawarecode.cnw.CnwLexer;
+
 public class CWMain {
     public static void main(String[] args) {
-        
+        CnwLexer cnwLexer = new CnwLexer();
+        cnwLexer.runLexer();
     }
 }
