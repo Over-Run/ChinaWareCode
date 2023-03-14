93e54f0bf5ff73ef2da4aa9a753c5ae2078f5962
baka4n <qq474899581@gmail.com>
2023-03-14 11:53:44 +0800

    删除老token和lexer

--- src/main/java/org/overrun/chinawarecode/CWMain.java
+++ src/main/java/org/overrun/chinawarecode/CWMain.java
@@ -1,11 +1,7 @@
 package org.overrun.chinawarecode;
 
-
-import org.overrun.chinawarecode.cnw.CnwLexer;
-
 public class CWMain {
     public static void main(String[] args) {
-        CnwLexer cnwLexer = new CnwLexer();
-        cnwLexer.runLexer();
+
     }
 }
